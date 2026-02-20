6142847ad0caac09b9e5d9488d80bdb8c596dd5b
NEWGAMTAN <cmw0530@echomarketing.co.kr>
2026-02-20 20:29:57 +0900

    update ai assets 202602202027

--- ai_data.js
+++ ai_data.js
@@ -1,249 +1,144 @@
 var aiDataAll = {
-    "version": "202602202022",
-    "updated_at": "2026-02-20 20:24:55",
+    "version": "202602202027",
+    "updated_at": "2026-02-20 20:29:57",
     "data": {
-        "편안함_에어소프트": {
+        "편안함_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "편안함을(를) 위한 에어소프트",
+                    "main_copy": "편안함을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#스포츠",
-                        "#활동성"
+                        "#외출핏",
+                        "#캡내장"
                     ],
-                    "image": "ai_comfort_product_1.jpg",
+                    "image": "ai_comfort_easywear_1.jpg",
                     "product_codes": [
-                        "2086045",
-                        "2086046",
-                        "2086025",
-                        "2086026",
-                        "2086027",
-                        "2086029",
-                        "2086030"
+                        "2086140",
+                        "2086138"
                     ],
-                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                    "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "편안함을(를) 위한 에어소프트",
+                    "main_copy": "편안함을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#스포츠",
-                        "#활동성"
+                        "#골지원단",
+                        "#캡내장"
                     ],
-                    "image": "ai_comfort_product_2.jpg",
+                    "image": "ai_comfort_easywear_2.jpg",
                     "product_codes": [
-                        "2086045",
-                        "2086046",
-                        "2086025",
-                        "2086026",
-                        "2086027",
-                        "2086029",
-                        "2086030"
+                        "2086140",
+                        "2086138"
                     ],
-                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                    "product_number_text": "2086140 2086138"
+                },
+                {
+                    "main_copy": "편안함을(를) 위한 이지웨어",
+                    "cta_text": "자세히 보기",
+                    "hashtags": [
+                        "#골지원단",
+                        "#외출핏"
+                    ],
+                    "image": "ai_comfort_easywear_3.jpg",
+                    "product_codes": [
+                        "2086140",
+                        "2086138"
+                    ],
+                    "product_number_text": "2086140 2086138"
                 }
             ],
             "usp_class": "comfort"
         },
-        "편안함_에어스윙": {
+        "특가_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "편안함을(를) 위한 에어스윙",
+                    "main_copy": "특가을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#프로개발",
-                        "#활동성"
+                        "#보풀방지",
+                        "#외출핏"
                     ],
-                    "image": "ai_comfort_air-swing_1.jpg",
+                    "image": "ai_special-price_easywear_1.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
+                    "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "편안함을(를) 위한 에어스윙",
+                    "main_copy": "특가을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#활동성",
-                        "#프로개발"
+                        "#보풀방지",
+                        "#외출핏"
                     ],
-                    "image": "ai_comfort_air-swing_2.jpg",
+                    "image": "ai_special-price_easywear_2.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
+                    "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "편안함을(를) 위한 에어스윙",
+                    "main_copy": "특가을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#활동성",
-                        "#스포츠"
+                        "#보풀방지",
+                        "#골지원단"
                     ],
-                    "image": "ai_comfort_air-swing_3.jpeg",
+                    "image": "ai_special-price_easywear_3.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
-                }
-            ],
-            "usp_class": "comfort"
-        },
-        "특가_에어소프트": {
-            "pairs": [
-                {
-                    "main_copy": "특가을(를) 위한 에어소프트",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#스포츠",
-                        "#활동성"
-                    ],
-                    "image": "ai_special-price_product_1.jpg",
-                    "product_codes": [
-                        "2086045",
-                        "2086046",
-                        "2086025",
-                        "2086026",
-                        "2086027",
-                        "2086029",
-                        "2086030"
-                    ],
-                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
-                },
-                {
-                    "main_copy": "특가을(를) 위한 에어소프트",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#활동성",
-                        "#프로개발"
-                    ],
-                    "image": "ai_special-price_product_2.jpg",
-                    "product_codes": [
-                        "2086045",
-                        "2086046",
-                        "2086025",
-                        "2086026",
-                        "2086027",
-                        "2086029",
-                        "2086030"
-                    ],
-                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
-                },
-                {
-                    "main_copy": "특가을(를) 위한 에어소프트",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#프로개발",
-                        "#활동성"
-                    ],
-                    "image": "ai_special-price_product_3.jpg",
-                    "product_codes": [
-                        "2086045",
-                        "2086046",
-                        "2086025",
-                        "2086026",
-                        "2086027",
-                        "2086029",
-                        "2086030"
-                    ],
-                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                    "product_number_text": "2086140 2086138"
                 }
             ],
             "usp_class": "special-price"
         },
-        "특가_에어스윙": {
+        "내구성_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "특가을(를) 위한 에어스윙",
+                    "main_copy": "내구성을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#프로개발",
-                        "#활동성"
+                        "#보풀방지",
+                        "#외출핏"
                     ],
-                    "image": "ai_special-price_air-swing_1.jpg",
+                    "image": "ai_durability_easywear_1.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
+                    "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "특가을(를) 위한 에어스윙",
+                    "main_copy": "내구성을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#프로개발",
-                        "#스포츠"
+                        "#외출핏",
+                        "#골지원단"
                     ],
-                    "image": "ai_special-price_air-swing_2.jpg",
+                    "image": "ai_durability_easywear_2.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
+                    "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "특가을(를) 위한 에어스윙",
+                    "main_copy": "내구성을(를) 위한 이지웨어",
                     "cta_text": "자세히 보기",
                     "hashtags": [
-                        "#프로개발",
-                        "#활동성"
+                        "#보풀방지",
+                        "#골지원단"
                     ],
-                    "image": "ai_special-price_air-swing_3.jpg",
+                    "image": "ai_durability_easywear_3.jpg",
                     "product_codes": [
-                        "2085981",
+                        "2086140",
                         "2086138"
                     ],
-                    "product_number_text": "2085981 2086138"
-                }
-            ],
-            "usp_class": "special-price"
-        },
-        "내구성_에어스윙": {
-            "pairs": [
-                {
-                    "main_copy": "내구성을(를) 위한 에어스윙",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#프로개발",
-                        "#흔들림방지"
-                    ],
-                    "image": "ai_durability_air-swing_1.jpg",
-                    "product_codes": [
-                        "2085981",
-                        "2086138"
-                    ],
-                    "product_number_text": "2085981 2086138"
-                },
-                {
-                    "main_copy": "내구성을(를) 위한 에어스윙",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#활동성",
-                        "#프로개발"
-                    ],
-                    "image": "ai_durability_air-swing_2.jpg",
-                    "product_codes": [
-                        "2085981",
-                        "2086138"
-                    ],
-                    "product_number_text": "2085981 2086138"
-                },
-                {
-                    "main_copy": "내구성을(를) 위한 에어스윙",
-                    "cta_text": "자세히 보기",
-                    "hashtags": [
-                        "#스포츠",
-                        "#프로개발"
-                    ],
-                    "image": "ai_durability_air-swing_3.jpg",
-                    "product_codes": [
-                        "2085981",
-                        "2086138"
-                    ],
-                    "product_number_text": "2085981 2086138"
+                    "product_number_text": "2086140 2086138"
                 }
             ],
             "usp_class": "durability"
@@ -259,8 +154,8 @@ var aiDataAll = {
             "2086029",
             "2086030"
         ],
-        "에어스윙": [
-            "2085981",
+        "이지웨어": [
+            "2086140",
             "2086138"
         ]
     }
