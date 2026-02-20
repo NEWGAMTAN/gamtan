91dabe9e8843a188b87d0a5070f4975e40febc74
NEWGAMTAN <cmw0530@echomarketing.co.kr>
2026-02-20 20:59:21 +0900

    update ai assets 202602202057

--- ai_data.js
+++ ai_data.js
@@ -1,14 +1,14 @@
 var aiDataAll = {
-    "version": "202602202040",
-    "updated_at": "2026-02-20 20:42:37",
+    "version": "202602202057",
+    "updated_at": "2026-02-20 20:59:21",
     "data": {
         "편안함_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "자연스러운 라인\n외출복으로 손색 없는 핏",
+                    "main_copy": "편안함을 입다\n이지웨어의 매력",
                     "hashtags": [
                         "#캡내장",
-                        "#매끈함"
+                        "#외출핏"
                     ],
                     "image": "ai_comfort_easywear_1.jpg",
                     "product_codes": [
@@ -18,10 +18,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "데일리 편안함\n소장 가치 이지웨어",
+                    "main_copy": "믿고 입는 편안함\n이지웨어의 매끈함",
                     "hashtags": [
-                        "#보풀방지",
-                        "#외출핏"
+                        "#외출핏",
+                        "#캡내장"
                     ],
                     "image": "ai_comfort_easywear_2.jpg",
                     "product_codes": [
@@ -31,10 +31,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "일상을 여유롭게\n감탄의 이지웨어",
+                    "main_copy": "기분 좋은 매일의 시작\n여성 프리미엄 이지웨어",
                     "hashtags": [
-                        "#외출핏",
-                        "#보풀방지"
+                        "#매끈함",
+                        "#외출핏"
                     ],
                     "image": "ai_comfort_easywear_3.jpg",
                     "product_codes": [
@@ -46,10 +46,51 @@ var aiDataAll = {
             ],
             "usp_class": "comfort"
         },
+        "특가_에어소프트": {
+            "pairs": [
+                {
+                    "main_copy": "변치 않는 우아함\n특가로 만나는 에어소프트",
+                    "hashtags": [
+                        "#베스트셀러",
+                        "#부드러움"
+                    ],
+                    "image": "ai_special-price_air-soft_1.jpg",
+                    "product_codes": [
+                        "2086045",
+                        "2086046",
+                        "2086025",
+                        "2086026",
+                        "2086027",
+                        "2086029",
+                        "2086030"
+                    ],
+                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                },
+                {
+                    "main_copy": "변치 않는 세련미\n슬기로운 F/W 소장",
+                    "hashtags": [
+                        "#내구성",
+                        "#부드러움"
+                    ],
+                    "image": "ai_special-price_air-soft_2.jpg",
+                    "product_codes": [
+                        "2086045",
+                        "2086046",
+                        "2086025",
+                        "2086026",
+                        "2086027",
+                        "2086029",
+                        "2086030"
+                    ],
+                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                }
+            ],
+            "usp_class": "special-price"
+        },
         "특가_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "입는 순간 특별함\n이지웨어의 대담한 핏",
+                    "main_copy": "특별한 날의 선택\n이지웨어의 외출패션",
                     "hashtags": [
                         "#캡내장",
                         "#외출핏"
@@ -62,10 +103,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "특별한 데일리 이지웨어\n캡 내장 골지원단의 매력",
+                    "main_copy": "특가로 만나는 기회\n소장 가치 이지웨어",
                     "hashtags": [
-                        "#외출핏",
-                        "#보풀방지"
+                        "#캡내장",
+                        "#외출핏"
                     ],
                     "image": "ai_special-price_easywear_2.jpg",
                     "product_codes": [
@@ -75,7 +116,7 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "어떤 순간에도 완벽\n캡 내장형 골지 원단",
+                    "main_copy": "빈틈없는 매끈함\n캡 내장형 이지웨어",
                     "hashtags": [
                         "#외출핏",
                         "#보풀방지"
@@ -90,10 +131,51 @@ var aiDataAll = {
             ],
             "usp_class": "special-price"
         },
+        "내구성_에어소프트": {
+            "pairs": [
+                {
+                    "main_copy": "견고한 아름다움\n감탄브라 에어소프트",
+                    "hashtags": [
+                        "#내구성",
+                        "#부드러움"
+                    ],
+                    "image": "ai_durability_air-soft_1.jpg",
+                    "product_codes": [
+                        "2086045",
+                        "2086046",
+                        "2086025",
+                        "2086026",
+                        "2086027",
+                        "2086029",
+                        "2086030"
+                    ],
+                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                },
+                {
+                    "main_copy": "부드러운 실크 감촉\n가치를 더하는 내구성",
+                    "hashtags": [
+                        "#베스트셀러",
+                        "#부드러움"
+                    ],
+                    "image": "ai_durability_air-soft_2.jpg",
+                    "product_codes": [
+                        "2086045",
+                        "2086046",
+                        "2086025",
+                        "2086026",
+                        "2086027",
+                        "2086029",
+                        "2086030"
+                    ],
+                    "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                }
+            ],
+            "usp_class": "durability"
+        },
         "내구성_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "감탄할 내구성\n절제된 매끈함의 미학",
+                    "main_copy": "프리미엄의 새로운 기준\n견고한 내구성의 이지웨어",
                     "hashtags": [
                         "#캡내장",
                         "#외출핏"
@@ -106,10 +188,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "우아한 매일의 시작\n프리미엄 이지웨어",
+                    "main_copy": "완벽한 외출 준비\n캡 내장 골지원단 매력",
                     "hashtags": [
-                        "#매끈함",
-                        "#외출핏"
+                        "#외출핏",
+                        "#보풀방지"
                     ],
                     "image": "ai_durability_easywear_2.jpg",
                     "product_codes": [
@@ -119,10 +201,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "내구성의 새로운 정의\n프리미엄 런드리웨어",
+                    "main_copy": "캡 내장형 골지원단\n외출복의 완벽함",
                     "hashtags": [
                         "#외출핏",
-                        "#골지원단"
+                        "#보풀방지"
                     ],
                     "image": "ai_durability_easywear_3.jpg",
                     "product_codes": [
