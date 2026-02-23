07bb8ff27b89177bb9749337626d44397009215b
NEWGAMTAN <cmw0530@echomarketing.co.kr>
2026-02-23 10:26:23 +0900

    update ai assets 202602231026

--- ai_data.js
+++ ai_data.js
@@ -1,11 +1,11 @@
 var aiDataAll = {
-    "version": "202602202101",
-    "updated_at": "2026-02-20 21:01:59",
+    "version": "202602231026",
+    "updated_at": "2026-02-23 10:26:22",
     "data": {
         "편안함_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "하루의 안락함\n외출복 같은 이지웨어",
+                    "main_copy": "편안함의 새로운 정의\n외출복 손색없는 핏",
                     "hashtags": [
                         "#캡내장",
                         "#외출핏"
@@ -18,10 +18,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "장시간에도 불편함 제로\n완벽한 외출핏 자랑",
+                    "main_copy": "편안함으로 시작되는\n외출복 이상의 경험",
                     "hashtags": [
                         "#캡내장",
-                        "#매끈함"
+                        "#외출핏"
                     ],
                     "image": "ai_comfort_easywear_2.jpg",
                     "product_codes": [
@@ -36,10 +36,10 @@ var aiDataAll = {
         "특가_에어소프트": {
             "pairs": [
                 {
-                    "main_copy": "특별한 날의 고급스러움\n변함없는 견고한 선택",
+                    "main_copy": "세탁 후에도 탄탄함\nF W 베스트셀러 에어소프트",
                     "hashtags": [
-                        "#부드러움",
-                        "#베스트셀러"
+                        "#내구성",
+                        "#부드러움"
                     ],
                     "image": "ai_special-price_air-soft_1.jpg",
                     "product_codes": [
@@ -54,10 +54,10 @@ var aiDataAll = {
                     "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
                 },
                 {
-                    "main_copy": "실크 같은 부드러움\nF/W 베스트셀러의 가치",
+                    "main_copy": "감동의 부드러움\nF/W 베스트셀러 에어소프트",
                     "hashtags": [
-                        "#내구성",
-                        "#복원력"
+                        "#복원력",
+                        "#내구성"
                     ],
                     "image": "ai_special-price_air-soft_2.jpg",
                     "product_codes": [
@@ -77,7 +77,7 @@ var aiDataAll = {
         "특가_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "지금이 바로 기회\n특별한 이지웨어 소장 가치",
+                    "main_copy": "특가로 만나는 기회\n소장 가치의 이지웨어",
                     "hashtags": [
                         "#캡내장",
                         "#외출핏"
@@ -90,10 +90,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "특별한 당신을 위해\n프리미엄 이지웨어",
+                    "main_copy": "지금, 경험하세요\n프리미엄 이지웨어",
                     "hashtags": [
-                        "#보풀방지",
-                        "#골지원단"
+                        "#캡내장",
+                        "#외출핏"
                     ],
                     "image": "ai_special-price_easywear_2.jpg",
                     "product_codes": [
@@ -108,10 +108,10 @@ var aiDataAll = {
         "내구성_에어소프트": {
             "pairs": [
                 {
-                    "main_copy": "견고한 내구성\n에어소프트의 소장 가치",
+                    "main_copy": "변화 없는 견고함\n에어소프트의 소장 가치",
                     "hashtags": [
-                        "#부드러움",
-                        "#베스트셀러"
+                        "#내구성",
+                        "#부드러움"
                     ],
                     "image": "ai_durability_air-soft_1.jpg",
                     "product_codes": [
@@ -126,10 +126,10 @@ var aiDataAll = {
                     "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
                 },
                 {
-                    "main_copy": "실크처럼 부드럽게\n안정적인 내구성",
+                    "main_copy": "실크 같은 감촉\n세탁 후에도 견고함",
                     "hashtags": [
-                        "#베스트셀러",
-                        "#부드러움"
+                        "#내구성",
+                        "#베스트셀러"
                     ],
                     "image": "ai_durability_air-soft_2.jpg",
                     "product_codes": [
