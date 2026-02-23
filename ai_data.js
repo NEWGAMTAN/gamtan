c7dad79b7407a1467cc80a54e249494cfa2e3ffd
NEWGAMTAN <cmw0530@echomarketing.co.kr>
2026-02-23 11:43:01 +0900

    update ai assets 202602231142

--- ai_data.js
+++ ai_data.js
@@ -1,14 +1,14 @@
 var aiDataAll = {
-    "version": "202602231026",
-    "updated_at": "2026-02-23 10:26:22",
+    "version": "202602231142",
+    "updated_at": "2026-02-23 11:43:01",
     "data": {
         "편안함_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "편안함의 새로운 정의\n외출복 손색없는 핏",
+                    "main_copy": "견고한 내구성\n이지웨어의 소장 가치",
                     "hashtags": [
-                        "#캡내장",
-                        "#외출핏"
+                        "#외출핏",
+                        "#보풀방지"
                     ],
                     "image": "ai_comfort_easywear_1.jpg",
                     "product_codes": [
@@ -18,10 +18,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "편안함으로 시작되는\n외출복 이상의 경험",
+                    "main_copy": "진정한 편안함\n외출복으로 손색없는 핏",
                     "hashtags": [
                         "#캡내장",
-                        "#외출핏"
+                        "#보풀방지"
                     ],
                     "image": "ai_comfort_easywear_2.jpg",
                     "product_codes": [
@@ -36,10 +36,10 @@ var aiDataAll = {
         "특가_에어소프트": {
             "pairs": [
                 {
-                    "main_copy": "세탁 후에도 탄탄함\nF W 베스트셀러 에어소프트",
+                    "main_copy": "에어소프트의 선택\n실크 같은 프리미엄",
                     "hashtags": [
-                        "#내구성",
-                        "#부드러움"
+                        "#부드러움",
+                        "#베스트셀러"
                     ],
                     "image": "ai_special-price_air-soft_1.jpg",
                     "product_codes": [
@@ -54,10 +54,10 @@ var aiDataAll = {
                     "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
                 },
                 {
-                    "main_copy": "감동의 부드러움\nF/W 베스트셀러 에어소프트",
+                    "main_copy": "변치 않는 가치를\n최고의 선택, 에어소프트",
                     "hashtags": [
-                        "#복원력",
-                        "#내구성"
+                        "#부드러움",
+                        "#베스트셀러"
                     ],
                     "image": "ai_special-price_air-soft_2.jpg",
                     "product_codes": [
@@ -77,9 +77,9 @@ var aiDataAll = {
         "특가_이지웨어": {
             "pairs": [
                 {
-                    "main_copy": "특가로 만나는 기회\n소장 가치의 이지웨어",
+                    "main_copy": "매일의 필수템\n견고한 매끈함의 소장 가치",
                     "hashtags": [
-                        "#캡내장",
+                        "#골지원단",
                         "#외출핏"
                     ],
                     "image": "ai_special-price_easywear_1.jpg",
@@ -90,10 +90,10 @@ var aiDataAll = {
                     "product_number_text": "2086140 2086138"
                 },
                 {
-                    "main_copy": "지금, 경험하세요\n프리미엄 이지웨어",
+                    "main_copy": "편안함의 새로운 정의\n이지웨어의 매력적 핏",
                     "hashtags": [
-                        "#캡내장",
-                        "#외출핏"
+                        "#외출핏",
+                        "#보풀방지"
                     ],
                     "image": "ai_special-price_easywear_2.jpg",
                     "product_codes": [
@@ -108,10 +108,10 @@ var aiDataAll = {
         "내구성_에어소프트": {
             "pairs": [
                 {
-                    "main_copy": "변화 없는 견고함\n에어소프트의 소장 가치",
+                    "main_copy": "비교할 수 없는 탁월함\n부드러움의 새로운 기준",
                     "hashtags": [
                         "#내구성",
-                        "#부드러움"
+                        "#베스트셀러"
                     ],
                     "image": "ai_durability_air-soft_1.jpg",
                     "product_codes": [
@@ -126,10 +126,10 @@ var aiDataAll = {
                     "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
                 },
                 {
-                    "main_copy": "실크 같은 감촉\n세탁 후에도 견고함",
+                    "main_copy": "견고한 안심\n에어소프트의 부드러움",
                     "hashtags": [
-                        "#내구성",
-                        "#베스트셀러"
+                        "#베스트셀러",
+                        "#내구성"
                     ],
                     "image": "ai_durability_air-soft_2.jpg",
                     "product_codes": [
@@ -142,6 +142,24 @@ var aiDataAll = {
                         "2086030"
                     ],
                     "product_number_text": "2086045 2086046 2086025 2086026 2086027 2086029 2086030"
+                }
+            ],
+            "usp_class": "durability"
+        },
+        "내구성_이지웨어": {
+            "pairs": [
+                {
+                    "main_copy": "지지력을 채우다\n프리미엄 내구성 이지웨어",
+                    "hashtags": [
+                        "#캡내장",
+                        "#외출핏"
+                    ],
+                    "image": "ai_durability_easywear_1.jpg",
+                    "product_codes": [
+                        "2086140",
+                        "2086138"
+                    ],
+                    "product_number_text": "2086140 2086138"
                 }
             ],
             "usp_class": "durability"
