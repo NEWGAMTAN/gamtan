ee7bed17dfa5a4d85e6556744085143eda4aa0c9
차민우 <cma_product@echomarketing.co.kr>
2026-05-04 19:46:51 +0900

    wip: PALS Lefee

---
+++ pals_lefee.js
@@ -0,0 +1,521 @@
+var aiDataAll = {
+    "version": "202605041944",
+    "updated_at": "2026-05-04 19:45:14",
+    "data": {
+        "특가_이더": {
+            "main_copy": "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요.",
+            "main_copies": [
+                "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요."
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_special-price_aether_1.jpg"
+                },
+                {
+                    "image": "pals_special-price_aether_2.jpg"
+                },
+                {
+                    "image": "pals_special-price_aether_3.jpg"
+                }
+            ],
+            "usp_class": "special-price"
+        },
+        "특가_아이리스": {
+            "main_copy": "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요.",
+            "main_copies": [
+                "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요."
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_special-price_iris_1.jpg"
+                },
+                {
+                    "image": "pals_special-price_iris_2.jpg"
+                },
+                {
+                    "image": "pals_special-price_iris_3.jpg"
+                }
+            ],
+            "usp_class": "special-price"
+        },
+        "특가_헤일로": {
+            "main_copy": "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요.",
+            "main_copies": [
+                "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요."
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_special-price_halo_1.jpg"
+                },
+                {
+                    "image": "pals_special-price_halo_2.jpg"
+                },
+                {
+                    "image": "pals_special-price_halo_3.jpg"
+                }
+            ],
+            "usp_class": "special-price"
+        },
+        "특가_루스": {
+            "main_copy": "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요.",
+            "main_copies": [
+                "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요."
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_special-price_luce_1.jpg"
+                },
+                {
+                    "image": "pals_special-price_luce_2.jpg"
+                },
+                {
+                    "image": "pals_special-price_luce_3.jpg"
+                }
+            ],
+            "usp_class": "special-price"
+        },
+        "특가_레이지": {
+            "main_copy": "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요.",
+            "main_copies": [
+                "26 S/S\n큰 컵 만을 위한\n가벼운 여름을 준비했어요."
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_special-price_lazy_1.jpg"
+                },
+                {
+                    "image": "pals_special-price_lazy_2.jpg"
+                },
+                {
+                    "image": "pals_special-price_lazy_3.jpg"
+                }
+            ],
+            "usp_class": "special-price"
+        },
+        "디자인_이더": {
+            "main_copy": "큰 컵도 취향대로\n시크한 디자인을\n골라보세요",
+            "main_copies": [
+                "큰 컵도 취향대로\n시크한 디자인을\n골라보세요",
+                "트렌디한 감성으로\n큰 컵을 위한\n디자인을 만나보세요",
+                "취향에 맞춘\n예쁜 디테일을\n선택해보세요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_design_aether_1.jpg"
+                },
+                {
+                    "image": "pals_design_aether_2.jpg"
+                },
+                {
+                    "image": "pals_design_aether_3.jpg"
+                }
+            ],
+            "usp_class": "design"
+        },
+        "디자인_아이리스": {
+            "main_copy": "트렌디하고\n예쁜 디자인을\n입어보세요",
+            "main_copies": [
+                "트렌디하고\n예쁜 디자인을\n입어보세요",
+                "취향에 맞는\n디자인으로\n눈길을 사로잡아요",
+                "은은한 레이스로\n취향에 맞는 무드까지\n디자인을 완성하세요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_design_iris_1.jpg"
+                },
+                {
+                    "image": "pals_design_iris_2.jpg"
+                },
+                {
+                    "image": "pals_design_iris_3.jpg"
+                }
+            ],
+            "usp_class": "design"
+        },
+        "디자인_헤일로": {
+            "main_copy": "큰 컵도 취향대로\n시크한 디자인을\n즐겨보세요",
+            "main_copies": [
+                "큰 컵도 취향대로\n시크한 디자인을\n즐겨보세요",
+                "트렌디한 매력,\n예쁜 디테일로\n만나보세요",
+                "눈에 딱 맞는\n디자인을 찾아\n입어보세요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_design_halo_1.jpg"
+                },
+                {
+                    "image": "pals_design_halo_2.jpg"
+                },
+                {
+                    "image": "pals_design_halo_3.jpg"
+                }
+            ],
+            "usp_class": "design"
+        },
+        "디자인_루스": {
+            "main_copy": "트렌디하게 입고\n예쁜 디테일까지\n완성하세요",
+            "main_copies": [
+                "트렌디하게 입고\n예쁜 디테일까지\n완성하세요",
+                "큰 컵도 취향대로\n시크한 스타일로\n입어보세요",
+                "당신의 멋을\n디자인으로\n표현하세요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_design_luce_1.jpg"
+                },
+                {
+                    "image": "pals_design_luce_2.jpg"
+                },
+                {
+                    "image": "pals_design_luce_3.jpg"
+                }
+            ],
+            "usp_class": "design"
+        },
+        "디자인_레이지": {
+            "main_copy": "큰 컵도 취향대로\n시크하고 트렌디한\n디자인을 골라보세요",
+            "main_copies": [
+                "큰 컵도 취향대로\n시크하고 트렌디한\n디자인을 골라보세요",
+                "예쁜 디테일까지\n큰 컵에 어울리는\n디자인을 만나보세요",
+                "취향이 분명하다면\n눈에 바로 들어오는\n디자인을 입어보세요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_design_lazy_1.jpg"
+                },
+                {
+                    "image": "pals_design_lazy_2.jpg"
+                },
+                {
+                    "image": "pals_design_lazy_3.jpg"
+                }
+            ],
+            "usp_class": "design"
+        },
+        "보정력_이더": {
+            "main_copy": "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+            "main_copies": [
+                "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+                "상체 라인까지\n옆구리 군살까지\n한 번에 정리해요",
+                "날씬한 핏으로\n큰 컵 실루엣까지\n부드럽게 정리해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_shaping_aether_1.jpg"
+                },
+                {
+                    "image": "pals_shaping_aether_2.jpg"
+                },
+                {
+                    "image": "pals_shaping_aether_3.jpg"
+                }
+            ],
+            "usp_class": "shaping"
+        },
+        "보정력_아이리스": {
+            "main_copy": "옷핏이 달라지게\n옆구리 군살까지\n부드럽게 정리해요",
+            "main_copies": [
+                "옷핏이 달라지게\n옆구리 군살까지\n부드럽게 정리해요",
+                "부유방까지 매끈하게\n상체 라인까지\n한 번에 정리해요",
+                "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_shaping_iris_1.jpg"
+                },
+                {
+                    "image": "pals_shaping_iris_2.jpg"
+                },
+                {
+                    "image": "pals_shaping_iris_3.jpg"
+                }
+            ],
+            "usp_class": "shaping"
+        },
+        "보정력_헤일로": {
+            "main_copy": "옷핏이 달라지게\n부유방, 군살까지\n한 번에 정리해요",
+            "main_copies": [
+                "옷핏이 달라지게\n부유방, 군살까지\n한 번에 정리해요",
+                "날씬한 핏으로\n큰 컵 실루엣까지\n부드럽게 정리해요",
+                "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_shaping_halo_1.jpg"
+                },
+                {
+                    "image": "pals_shaping_halo_2.jpg"
+                },
+                {
+                    "image": "pals_shaping_halo_3.jpg"
+                }
+            ],
+            "usp_class": "shaping"
+        },
+        "보정력_루스": {
+            "main_copy": "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+            "main_copies": [
+                "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+                "상체 라인까지\n옆구리 군살까지\n한 번에 정리해요",
+                "날씬한 핏으로\n큰 컵 실루엣까지\n부드럽게 정리해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_shaping_luce_1.jpg"
+                },
+                {
+                    "image": "pals_shaping_luce_2.jpg"
+                },
+                {
+                    "image": "pals_shaping_luce_3.jpg"
+                }
+            ],
+            "usp_class": "shaping"
+        },
+        "보정력_레이지": {
+            "main_copy": "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+            "main_copies": [
+                "옷핏이 달라지게\n군살과 부유방까지\n매끈하게 정리해요",
+                "상체 라인까지\n옆구리 군살까지\n한 번에 정리해요",
+                "날씬한 핏으로\n큰 컵 실루엣까지\n부드럽게 정리해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_shaping_lazy_1.jpg"
+                },
+                {
+                    "image": "pals_shaping_lazy_2.jpg"
+                },
+                {
+                    "image": "pals_shaping_lazy_3.jpg"
+                }
+            ],
+            "usp_class": "shaping"
+        },
+        "지지력_이더": {
+            "main_copy": "무게를 나눠 받아\n큰 컵을 안정감 있게\n받쳐서 입게 해요",
+            "main_copies": [
+                "무게를 나눠 받아\n큰 컵을 안정감 있게\n받쳐서 입게 해요",
+                "어깨 부담 줄이게\n무게를 고르게 나눠\n안정감 있게 입어요",
+                "구부정한 어깨도\n아래서 위로 탄탄히\n받쳐서 편히 입어요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_support_aether_1.jpg"
+                },
+                {
+                    "image": "pals_support_aether_2.jpg"
+                },
+                {
+                    "image": "pals_support_aether_3.jpg"
+                }
+            ],
+            "usp_class": "support"
+        },
+        "지지력_아이리스": {
+            "main_copy": "어깨에 부담 없이\n무게를 고르게 분산해\n편히 입으세요",
+            "main_copies": [
+                "어깨에 부담 없이\n무게를 고르게 분산해\n편히 입으세요",
+                "어깨 부담 덜고\n안정감 있게 받쳐\n당당히 입으세요",
+                "무게를 나눠 받아\n큰 컵을 안정감 있게\n받쳐서 입게 해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_support_iris_1.jpg"
+                },
+                {
+                    "image": "pals_support_iris_2.jpg"
+                },
+                {
+                    "image": "pals_support_iris_3.jpg"
+                }
+            ],
+            "usp_class": "support"
+        },
+        "지지력_헤일로": {
+            "main_copy": "무게를 분산해\n큰 컵을 안정감\n있게 입어요",
+            "main_copies": [
+                "무게를 분산해\n큰 컵을 안정감\n있게 입어요",
+                "구부정한 어깨를\n탄탄히 받쳐서\n안정감 있게 입어요",
+                "어깨 부담 덜고\n구부정한 어깨도\n탄탄하게 받쳐요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_support_halo_1.jpg"
+                },
+                {
+                    "image": "pals_support_halo_2.jpg"
+                },
+                {
+                    "image": "pals_support_halo_3.jpg"
+                }
+            ],
+            "usp_class": "support"
+        },
+        "지지력_루스": {
+            "main_copy": "구부정한 어깨도\n탄탄히 받쳐줘요\n안심하고 입어요",
+            "main_copies": [
+                "구부정한 어깨도\n탄탄히 받쳐줘요\n안심하고 입어요",
+                "무게를 나눠 받아\n큰 컵을 안정감 있게\n받쳐서 입게 해요",
+                "구부정한 어깨도\n아래서 위로 탄탄히\n받쳐서 편히 입어요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_support_luce_1.jpg"
+                },
+                {
+                    "image": "pals_support_luce_2.jpg"
+                },
+                {
+                    "image": "pals_support_luce_3.jpg"
+                }
+            ],
+            "usp_class": "support"
+        },
+        "지지력_레이지": {
+            "main_copy": "어깨의 부담을 덜고\n단단하게 받쳐서\n안정감 있게 입어요",
+            "main_copies": [
+                "어깨의 부담을 덜고\n단단하게 받쳐서\n안정감 있게 입어요",
+                "구부정한 어깨도\n듬직하게 받쳐서\n부담 없이 입어요",
+                "무게를 나눠 받아\n큰 컵을 안정감 있게\n받쳐서 입게 해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_support_lazy_1.jpg"
+                },
+                {
+                    "image": "pals_support_lazy_2.jpg"
+                },
+                {
+                    "image": "pals_support_lazy_3.jpg"
+                }
+            ],
+            "usp_class": "support"
+        },
+        "편안함_이더": {
+            "main_copy": "조임 걱정 없이\n하루 오래 입어도\n편안하게 감싸줘요",
+            "main_copies": [
+                "조임 걱정 없이\n하루 오래 입어도\n편안하게 감싸줘요",
+                "피부에 닿을 때도\n부드러운 착용감이\n오래 이어지게 해요",
+                "압박감 줄여서\n장시간 입고 있어도\n편안하게 입게 해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_comfort_aether_1.jpg"
+                },
+                {
+                    "image": "pals_comfort_aether_2.jpg"
+                },
+                {
+                    "image": "pals_comfort_aether_3.jpg"
+                }
+            ],
+            "usp_class": "comfort"
+        },
+        "편안함_아이리스": {
+            "main_copy": "조임 없이\n하루 종일\n편안해요",
+            "main_copies": [
+                "조임 없이\n하루 종일\n편안해요",
+                "부드러운\n착용감이\n느껴져요",
+                "조임 걱정 없이\n하루 오래 입어도\n편안하게 감싸줘요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_comfort_iris_1.jpg"
+                },
+                {
+                    "image": "pals_comfort_iris_2.jpg"
+                },
+                {
+                    "image": "pals_comfort_iris_3.jpg"
+                }
+            ],
+            "usp_class": "comfort"
+        },
+        "편안함_헤일로": {
+            "main_copy": "피부에 닿을 때도\n부드러운 착용감이\n오래 이어지게 해요",
+            "main_copies": [
+                "피부에 닿을 때도\n부드러운 착용감이\n오래 이어지게 해요",
+                "압박감 줄여서\n장시간 입고 있어도\n편안하게 입게 해요",
+                "조임 걱정 없이\n하루 오래 입어도\n편안하게 감싸줘요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_comfort_halo_1.jpg"
+                },
+                {
+                    "image": "pals_comfort_halo_2.jpg"
+                },
+                {
+                    "image": "pals_comfort_halo_3.jpg"
+                }
+            ],
+            "usp_class": "comfort"
+        },
+        "편안함_루스": {
+            "main_copy": "조임 걱정 없이\n편안하게 감싸줘요\n하루 종일 느껴요",
+            "main_copies": [
+                "조임 걱정 없이\n편안하게 감싸줘요\n하루 종일 느껴요",
+                "피부에 닿을 때도\n부드러운 착용감이\n오래 이어지게 해요",
+                "장시간 입고 있어도\n압박감 줄여서\n편안하게 입게 해요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_comfort_luce_1.jpg"
+                },
+                {
+                    "image": "pals_comfort_luce_2.jpg"
+                },
+                {
+                    "image": "pals_comfort_luce_3.jpg"
+                }
+            ],
+            "usp_class": "comfort"
+        },
+        "편안함_레이지": {
+            "main_copy": "부드러운 터치로\n편안함을 느껴보세요\n오래 입어도 좋아요",
+            "main_copies": [
+                "부드러운 터치로\n편안함을 느껴보세요\n오래 입어도 좋아요",
+                "가볍게, 편안하게\n조임 걱정 없이\n즐겨보세요",
+                "조임 걱정 없이\n하루 오래 입어도\n편안하게 감싸줘요"
+            ],
+            "meta_class": "case-a",
+            "pairs": [
+                {
+                    "image": "pals_comfort_lazy_1.jpg"
+                },
+                {
+                    "image": "pals_comfort_lazy_2.jpg"
+                },
+                {
+                    "image": "pals_comfort_lazy_3.jpg"
+                }
+            ],
+            "usp_class": "comfort"
+        }
+    }
+};
